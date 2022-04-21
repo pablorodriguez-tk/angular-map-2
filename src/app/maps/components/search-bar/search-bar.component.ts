d938dbd2cadb2225247dc45a1db4cc4b0d01c73c
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-04-20 21:43:19 -0300

    debounce on input

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent {
+  private debounceTimer?: NodeJS.Timer;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onQueryChanged(query: string) {
+    if (this.debounceTimer) {
+      clearTimeout(this.debounceTimer);
+    }
+    this.debounceTimer = setTimeout(() => {
+      console.log('debounce', query);
+    }, 350);
   }
-
 }
